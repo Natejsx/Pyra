4247e563b70391ee72f97623ee4d686a805d3b85
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-02-19 14:04:22 -0600

    fix: updated vitest to include workspaces

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,6 +1,7 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-  'packages/shared',
-  'packages/core',
+  'packages/shared/vitest.config.ts',
+  'packages/core/vitest.config.ts',
+  'packages/adapter-react/vitest.config.ts',
 ]);
