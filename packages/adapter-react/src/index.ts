b14e208e522664c02ae434e6299203fb9c092e13
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-04 01:02:33 -0600

    style: exported the new adapter-react functions

--- packages/adapter-react/src/index.ts
+++ packages/adapter-react/src/index.ts
@@ -4,5 +4,21 @@ export type { ImageProps } from "./Image.js";
 export { Link } from "./Link.js";
 export { NavLink } from "./NavLink.js";
 export type { NavLinkProps } from "./NavLink.js";
-export { useLocation, useNavigate, useSearchParams, useParams, useNavigating } from "./hooks.js";
-export type { Location } from "./hooks.js";
+export { Head } from "./Head.js";
+export type { HeadProps } from "./Head.js";
+export { ClientOnly } from "./ClientOnly.js";
+export type { ClientOnlyProps } from "./ClientOnly.js";
+export { Form } from "./Form.js";
+export type { FormProps } from "./Form.js";
+export {
+  useLocation,
+  useNavigate,
+  useSearchParams,
+  useParams,
+  useNavigating,
+  usePreload,
+  useBeforeNavigate,
+  useScrollRestoration,
+  useRouteError,
+} from "./hooks.js";
+export type { Location, RouteError } from "./hooks.js";
